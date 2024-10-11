249ac0c59f62c854e5992f714b6d672df7318b48
Kevin Pires <kevinpiresyt3@gmail.com>
2024-10-11 00:20:28 -0300

    atualição 2.0

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('specialButton').addEventListener('click', function() {
+    window.open('https://www.youtube.com/watch?v=dWpGsK8Md28', '_blank'); // Replace with your special URL
+});
